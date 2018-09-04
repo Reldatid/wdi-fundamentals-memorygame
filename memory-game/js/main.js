a60e8df872f1da1243826ac6f9f3825db5f68d33
Jonathan Odlum <jonathanodlum@hotmail.com>
2018-09-04 19:19:02 +1000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,17 @@
 console.log("Up And Running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['king', 'king', 'queen', 'queen'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+console.log( cardOne + " flipped");
+var cardTwo = cards[2];
+console.log( cardTwo + " flipped");
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!1");
+	}
+	else{
+		alert("Sorry, try again...");
+	}
+}
