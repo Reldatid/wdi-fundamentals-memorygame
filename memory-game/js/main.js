e38832944a3b1ede06e7b8433380d9cb047ab240
Jonathan Odlum <jonathanodlum@hotmail.com>
2018-09-05 19:23:46 +1000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,33 @@
 console.log("Up And Running!");
-var cards = ['king', 'king', 'queen', 'queen'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+console.log(cards);
 var cardsInPlay = [];
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId.rank]);
 	checkForMatch();
 }
 function checkForMatch() {
@@ -19,7 +43,4 @@ function checkForMatch() {
 		}
 	}
 }
-flipCard(0);
-flipCard(2);
-flipCard(0);
 flipCard(1);
